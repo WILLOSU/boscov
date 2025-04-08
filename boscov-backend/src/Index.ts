dfd35f8a3cc4e98e467c169d51708e7c6462842f
William Mota <william10sousamota@gmail.com>
2025-04-07 21:46:01 -0300

    feat - swagger criado, alterado README

--- boscov-backend/src/Index.ts
+++ boscov-backend/src/Index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
+
 dotenv.config(); // sempre no topo
 
 import authRoutes from './routes/AuthRoutes'; 
@@ -9,8 +10,13 @@ import userRoutes from './routes/UserRoutes';
 import filmeRoutes from './routes/FilmeRoutes';
 import generoRoutes from './routes/GeneroRoutes';
 import avaliacaoRoutes from './routes/AvaliacaoRoutes';
+import swaggerRoute from "./routes/SwaggerRoutes";
 
 const app = express();
+const swaggerUi = require('swagger-ui-express');
+const swaggerFile = require('./docs/swagger.json');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(cors());
 app.use(express.json()); 
@@ -21,6 +27,7 @@ app.use('/users', userRoutes);
 app.use('/api', filmeRoutes);
 app.use('/generos', generoRoutes);
 app.use('/avaliacao', avaliacaoRoutes);
+app.use('/doc', swaggerRoute);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
